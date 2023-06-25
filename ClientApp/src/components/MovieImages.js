8e729667cd208650a16cc31098aafb20e3ebed87
AHemenwayKC <38112533+AHemenwayKC@users.noreply.github.com>
2023-06-25 14:00:41 -0500

    Adding movie images

---
+++ ClientApp/src/components/MovieImages.js
@@ -0,0 +1,25 @@
+﻿import Godfather from '../images/Godfather.png';
+import Highlander from '../images/Highlander.png';
+import Highlander2 from '../images/Highlander2.png';
+import LastOfTheMohicans from '../images/LastOfTheMohicans.png';
+import PoliceAcademy6 from '../images/PoliceAcademy6.png';
+import RearWindow from '../images/RearWindow.png';
+import RoadHouse from '../images/RoadHouse.png';
+import Shawshank from '../images/Shawshank.png';
+import StarTrek4 from '../images/StarTrek4.png';
+import Superman4 from '../images/Superman4.png';
+
+const MovieImageArr = [
+    { id: 1, image: Godfather },
+    { id: 2, image: Highlander },
+    { id: 3, image: Highlander2 },
+    { id: 4, image: LastOfTheMohicans },
+    { id: 5, image: PoliceAcademy6 },
+    { id: 6, image: RearWindow },
+    { id: 7, image: RoadHouse },
+    { id: 8, image: Shawshank },
+    { id: 9, image: StarTrek4 },
+    { id: 10, image: Superman4 },
+];
+
+export default MovieImageArr;
--- ClientApp/src/components/RankItems.js
+++ ClientApp/src/components/RankItems.js
@@ -1,4 +1,5 @@
 ﻿import React, { useState, useEffect } from 'react';
+import MovieImageArr from './MovieImages.js';
 
 const RankItems = () => {
 
@@ -16,9 +17,14 @@ const RankItems = () => {
     },[])
 
     return (
-        <main> {
-            (items != null) ? items.map((item) => <h3>{item.title }</h3>):<div>Loading...</div>
+        <main>
+            <div className = "items-not-ranked"> {
+                (items.length > 0) ? items.map((item) =>
+                    <img id={`item-${item.id}`} src={MovieImageArr.find(o => o.id === item.imageId)?.image} />
+
+                    ) : <div>Loading...</div>
             }
+            </div>
         </main>
     )
 }
